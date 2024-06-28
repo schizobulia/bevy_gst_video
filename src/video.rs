d0dc889ca54aff32609d803380eb24bf71e69537
schizobulia <2833324528@qq.com>
2024-06-28 18:11:11 +0800

    feat: support audio play

--- src/video.rs
+++ src/video.rs
@@ -1,14 +1,16 @@
 extern crate gstreamer as gst;
 extern crate gstreamer_app as gst_app;
+extern crate gstreamer_audio as gst_audio;
 extern crate gstreamer_video as gst_video;
-
 use std::{
     collections::VecDeque,
     sync::{Arc, Mutex},
 };
 
+use byteorder::{ByteOrder, LittleEndian};
 use gst::{element_error, prelude::*};
 use gstreamer_video::VideoFrameExt;
+use rodio::OutputStream;
 
 pub struct VideoInfo {
     pub height: u32,
@@ -16,16 +18,20 @@ pub struct VideoInfo {
     pub data: Vec<u8>,
 }
 pub fn create_pipeline(ls: Arc<Mutex<VecDeque<VideoInfo>>>, video_rate: Arc<Mutex<f64>>) {
+    let (_stream, stream_handle) = OutputStream::try_default().expect("Error");
+    let player_audio = rodio::Sink::try_new(&stream_handle).expect("Error");
+
     gst::init().unwrap();
     let uri = "https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm";
     let pipeline = gst::parse::launch(&format!(
-        "uridecodebin uri={uri} ! videoconvert ! appsink name=sink"
+        "uridecodebin uri={uri} name=decodebin ! videoconvert ! appsink name=video_sink \
+ decodebin. ! audioconvert ! appsink name=audio_sink"
     ))
     .unwrap()
     .downcast::<gst::Pipeline>()
     .expect("Expected a gst::Pipeline");
     let appsink = pipeline
-        .by_name("sink")
+        .by_name("video_sink")
         .expect("Sink element not found")
         .downcast::<gst_app::AppSink>()
         .expect("Sink element is expected to be an appsink!");
@@ -85,8 +91,57 @@ pub fn create_pipeline(ls: Arc<Mutex<VecDeque<VideoInfo>>>, video_rate: Arc<Mute
             })
             .build(),
     );
-
+    let audio_sink = pipeline
+        .by_name("audio_sink")
+        .expect("Audio sink element not found")
+        .downcast::<gst_app::AppSink>()
+        .expect("Audio sink element is expected to be an appsink!");
     let bus = pipeline.bus().unwrap();
+
+    audio_sink.set_callbacks(
+        gst_app::AppSinkCallbacks::builder()
+            .new_sample(move |audio_sink| {
+                let sample = audio_sink
+                    .pull_sample()
+                    .map_err(|_| gst::FlowError::Eos)
+                    .expect("Error");
+                let buffer = sample
+                    .buffer()
+                    .ok_or_else(|| {
+                        element_error!(
+                            audio_sink,
+                            gst::ResourceError::Failed,
+                            ("Failed to get buffer from appsink")
+                        );
+                        gst::FlowError::Error
+                    })
+                    .expect("Error");
+                let caps = sample.caps().expect("Sample without caps");
+                let info = gst_audio::AudioInfo::from_caps(caps).expect("Failed to parse caps");
+                let map: gstreamer::BufferMap<gstreamer::buffer::Readable> =
+                    buffer.map_readable().map_err(|_| {
+                        element_error!(
+                            appsink,
+                            gst::ResourceError::Failed,
+                            ("Failed to map buffer readable")
+                        );
+
+                        gst::FlowError::Error
+                    })?;
+                let u8_data: &[u8] = map.as_slice();
+                let mut f32_data = vec![0f32; u8_data.len() / 4];
+                LittleEndian::read_f32_into(u8_data, &mut f32_data);
+
+                let ch = info.channels() as u16;
+                let rate = info.rate();
+                let s = rodio::buffer::SamplesBuffer::new(ch, rate, f32_data);
+                player_audio.append(s);
+
+                Ok(gst::FlowSuccess::Ok)
+            })
+            .build(),
+    );
+
     for msg in bus.iter_timed(gst::ClockTime::NONE) {
         use gst::MessageView;
         match msg.view() {
